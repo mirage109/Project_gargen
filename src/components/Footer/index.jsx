8d54e3d7535ab9545fa4e93eaba84a4b36a6f64d
Anastasiia Zibla <zibla.asia@gmail.com>
2023-09-07 17:11:45 +0200

    feat: urls to contact

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -10,21 +10,34 @@ export const Footer = () => {
       <div className={style.visitcard}>
         <div className={style.visitcardContact}>
           <h2 className={style.h2}>Contact</h2>
-          <p className={style.p}>+49 999 999 99 99</p>
+          <a href="tel:+4999999999" className={style.p}>
+            +49 999 999 99 99
+          </a>
           <div className={style.social}>
             <div className={style.socialbox}>
-              <img src={instagram} alt="insta" />
+              <a href="https://www.instagram.com/">
+                <img src={instagram} alt="insta" />
+              </a>
               <h6 className={style.h6}>Instagram</h6>
             </div>
             <div className={style.socialbox}>
-              <img src={whatssapp} alt="WhatsApp" />
+              <a href="tel:+4999999999">
+                <img src={whatssapp} alt="WhatsApp" />
+              </a>
               <h6 className={style.h6}>WhatsApp</h6>
             </div>
           </div>
         </div>
         <div className={style.visitcardAdress}>
           <h2 className={style.h2}>Address</h2>
-          <a href="https://www.google.com/search?q=telranDE" rel="noreferrer" target="_blank" className={style.address}>Linkstraße 2, 8 OG, 10785, Berlin, Deutschland</a>
+          <a
+            href="https://www.google.com/search?q=telranDE"
+            rel="noreferrer"
+            target="_blank"
+            className={style.address}
+          >
+            Linkstraße 2, 8 OG, 10785, Berlin, Deutschland
+          </a>
           <div className={style.workHours}>
             <h6 className={style.h6}>Working Hours:</h6>
             <p className={style.hours}>24 hours a day</p>
